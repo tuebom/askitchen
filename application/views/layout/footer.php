c661a5c9e0bdfe867407280909e8c4f965955576
k3r4zy <k3r4zy@hotmail.com>
2018-12-17 17:27:16 +0800

    update

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -5,12 +5,12 @@
 								<div class="col-md-3 footer-grid">
 									<h4>About Us</h4>
 									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-									<div class="social-icon">
+									<!-- <div class="social-icon">
 										<a href="#"><i class="icon"></i></a>
 										<a href="#"><i class="icon1"></i></a>
 										<a href="#"><i class="icon2"></i></a>
 										<a href="#"><i class="icon3"></i></a>
-									</div>
+									</div> -->
 								</div>
 								<div class="col-md-3 footer-grid">
 									<h4>Main Office</h4>
@@ -20,11 +20,11 @@
 										<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="#">(0361) 4727857</a></li>
 										<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="#">(0361) 4727855</a></li>
 									</ul>
-								</div>
+								</div>  
 								<div class="col-md-3 footer-grid">
 									<h4>Branch Office</h4>
 									<ul>
-										<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i><a href="#">Jl. Sungai Saddang Lama<br>Komplek Ruko Latanete Plaza<br>Blok B No. 11, Makassar - Indonesia</a></li>
+										<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i><a href="">Jl. Sungai Sadang Lama<br>Komplek Ruko Latanete Plaza<br>Blok B No. 11, Makassar - Indonesia</a></li>
 										<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="#">0851 0511 5557</a></li>
 										<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="#">0851 0813 3577</a></li>
 										<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="#">(0411) 822 4090</a></li>
@@ -33,9 +33,27 @@
 								<div class="col-md-3 footer-grid foot">
 									<h4>Follow Us</h4>
 									<ul>
-										<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i><a href="#">E Comertown Rd, Westby, USA</a></li>
+									<div class="social-icon">
+										<a href="http://www.facebook.com/askitchen" target="_blank"><i class="icon"></i></a>
+										<a href="http://www.instagram.com/askitchen" target="_blank"><i class="icon1"></i></a>
+										<!-- <a href="#"><i class="icon2"></i></a>
+										<a href="#"><i class="icon3"></i></a> -->
+									</div>
+										<!-- <li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i><a href="#">E Comertown Rd, Westby, USA</a></li>
 										<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="#">1 599-033-5036</a></li>
-										<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:example@mail.com"> example@mail.com</a></li>
+										<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:example@mail.com"> example@mail.com</a></li> -->
+									</ul>
+									<div class="col-md-3 footer-grid">
+									<h4>Our Groups </h4>
+									<ul>
+									<div class="logo-bawah">
+										<li><a href="http://www.askitchen.com/" target="_blank"><img class="img-footer" src="<?= site_url('images/askitchen_btm.png'); ?>" alt="ASkitchen Logo"></a></li>
+									
+									
+										<li><a href="http://www.asovic.co.id/" target="_blank"><img class="img-footer-2" src="<?= site_url('images/asovic_btm.png'); ?>" alt="ASovic Logo"></a></li>
+									
+										<li><a href="http://www.muchef.com/" target="_blank"><img class="img-footer-2" src="<?= site_url('images/muchef_btm.png'); ?>" alt="Muchef Logo"></li>
+									</div>
 									</ul>
 								</div>
 							<div class="clearfix"> </div>
@@ -44,13 +62,38 @@
 						</div>
 					</div>
 					<!---footer--->
+					<!-- <div class="footer-w3l">
+						<div class="container">
+							<div class="footer-grids">
+						<div class="col-md-3 footer-grid">
+									<h4>Our Groups :</h4>
+									<ul>
+									<div class="logo-bawah">
+										<li><a href="http://www.askitchen.com/" target="_blank"><img class="img-footer" src="<?= site_url('images/askitchen_btm.png'); ?>" alt="ASkitchen Logo"></a></li>
+									</div>
+									<div class="logo-bawah">
+										<li><a href="http://www.asovic.co.id/" target="_blank"><img class="img-footer-2" src="<?= site_url('images/asovic_btm.png'); ?>" alt="ASovic Logo"></a></li>
+									</div>
+									<div class="logo-bawah">
+										<li><a href="http://www.muchef.com/" target="_blank"><img class="img-footer-2" src="<?= site_url('images/muchef_btm.png'); ?>" alt="Muchef Logo"></li>
+									</div>
+									</ul>
+								</div> -->
+							<!-- <div class="copy-left">
+								<p>&copy; 2016 New Shop . All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+							</div>
+							<div class="copy-right"> -->
+							<div class="clearfix"></div>
+							</div>
+						</div>
+		</div>
 					<!--copy-->
 					<div class="copy-section">
 						<div class="container">
-							<div class="copy-left">
+							<!-- <div class="copy-left">
 								<p>&copy; 2016 New Shop . All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
 							</div>
-							<div class="copy-right">
+							<div class="copy-right"> -->
 								<img src="<?php echo site_url('images/card.png'); ?>" alt=""/>
 							</div>
 							<div class="clearfix"></div>
