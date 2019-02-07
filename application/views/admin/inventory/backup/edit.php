cbc1954ac27d222d75575a93ab26ec0488818e81
Putu Wirya <tuebom.wirya@gmail.com>
2019-02-07 17:15:35 +0800

    update inventory

--- application/views/admin/inventory/backup/edit.php
+++ application/views/admin/inventory/backup/edit.php
@@ -167,10 +167,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                             <?php echo lang('inventory_criteria', 'criteria', array('class' => 'col-sm-3 control-label')); ?>
                                             <div class="col-sm-9">
                                                 <select id="kriteria" name="kriteria" class="form-control">
-                                                    <option value=""<?=($this->data['kriteria'] == '')? ' selected="selected"' : ''; ?>>-</option>
-                                                    <option value="N"<?=($this->data['kriteria'] == 'N')? ' selected="selected"' : ''; ?>>New Products</option>
-                                                    <option value="P"<?=($this->data['kriteria'] == 'P')? ' selected="selected"' : ''; ?>>Promotion</option>
-                                                    <option value="B"<?=($this->data['kriteria'] == 'B')? ' selected="selected"' : ''; ?>>Best Seller</option>
+                                                    <option value="" selected>-</option>
+                                                    <option value="N">New Products</option>
+                                                    <option value="P">Promotion</option>
+                                                    <option value="B">Best Seller</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -195,10 +195,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                         <div class="form-group">
                                             <div class="col-sm-3"></div>
                                             <div class="col-sm-9">
-                                                <!-- The container for the uploaded files -->
-                                                <div id="files" class="files"><div class="product-img-edit"><img src="<?=site_url($this->data['products_dir'].'/'.$gambar['value'])?>" alt="Image"></div></div>
-                                                <a id="btnUpload" href="#" class="btn btn-sm btn-default btn-flat fileinput-button">Choose image
-                                                <input id="fileupload" type="file" name="files[]" multiple></a>
+                                                <a id="btnUpload" href="#" class="btn btn-sm btn-default btn-block btn-flat">Upload Gambar</a>
+                                                <?php echo form_upload($tmpgambar);?>
                                             </div>
                                         </div>
 
@@ -220,185 +218,69 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>
 
 <script type="text/javascript">
-$(document).ready(function() {
-
-    // Change this to the location of your server-side upload handler:
-    // var url = window.location.hostname === 'askitchen.com' ?
-    var url = <?php echo '"'.base_url('admin/fileupload').'"' ?>,
-        uploadButton = $('<button/>')
-            .addClass('btn btn-primary')
-            .prop('disabled', true)
-            .text('Processing...')
-            .on('click', function () {
-                var $this = $(this),
-                    data = $this.data();
-                $this
-                    .off('click')
-                    .text('Abort')
-                    .on('click', function () {
-                        $this.remove();
-                        data.abort();
-                    });
-                data.submit().always(function () {
-                    $this.remove();
-                });
-            });
-
-    $('#fileupload').fileupload({
-        url: url,
-        dataType: 'json',
-        autoUpload: false,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-        maxFileSize: 999000,
-        // Enable image resizing, except for Android and Opera,
-        // which actually support image resizing, but fail to
-        // send Blob objects via XHR requests:
-        disableImageResize: /Android(?!.*Chrome)|Opera/
-            .test(window.navigator.userAgent),
-        previewMaxWidth: 100,
-        previewMaxHeight: 100,
-        previewCrop: true
-    }).on('fileuploadadd', function (e, data) {
-        data.context = $('<div/>').appendTo('#files');
-        $.each(data.files, function (index, file) {
-            $('.product-img-edit').css('display', 'none');
-            var node = $('<p/>')
-                    .append($('<span/>').text(file.name));
-            if (!index) {
-                node
-                    .append('<br>')
-                    .append(uploadButton.clone(true).data(data));
-            }
-            node.appendTo(data.context);
-        });
-    }).on('fileuploadprocessalways', function (e, data) {
-        var index = data.index,
-            file = data.files[index],
-            node = $(data.context.children()[index]);
-        if (file.preview) {
-            node
-                .prepend('<br>')
-                .prepend(file.preview);
-        }
-        if (file.error) {
-            node
-                .append('<br>')
-                .append($('<span class="text-danger"/>').text(file.error));
-        }
-        if (index + 1 === data.files.length) {
-            data.context.find('button')
-                .text('Upload')
-                .prop('disabled', !!data.files.error);
-        }
-    }).on('fileuploadprogressall', function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
-    }).on('fileuploaddone', function (e, data) {
-        $.each(data.result.files, function (index, file) {
-            if (file.url) {
-                var link = $('<a>')
-                    .attr('target', '_blank')
-                    .prop('href', file.url);
-                $(data.context.children()[index])
-                    .wrap(link);
-            } else if (file.error) {
-                var error = $('<span class="text-danger"/>').text(file.error);
-                $(data.context.children()[index])
-                    .append('<br>')
-                    .append(error);
-            }
-        });
-    }).on('fileuploadfail', function (e, data) {
-        $.each(data.files, function (index) {
-            var error = $('<span class="text-danger"/>').text('File upload failed.');
-            $(data.context.children()[index])
-                .append('<br>')
-                .append(error);
-        });
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
+    $(document).ready(function() {
         
+        $('#btnSubmit').click(function(event) {
+            event.preventDefault();
+            $('#frmAddress').submit();
+        });
+        
+        $('#kdgol').change(function(){
+            $.ajax({
+                type: "POST",
+                url: "<?php echo site_url();?>inventory/level2/"+$(this).val(),
+                // dataType: "json",
+                // data: {"id":$(this).val()},
+                success:function(json){
+                    var data = json.data,
+                        firstid = data[0].kdgol2;
 
-    $('#kdgol').change(function(){
-        $.ajax({
-            type: "POST",
-            url: <?php echo '"'.site_url().'admin/inventory/level2/"' ?>+$(this).val(),
-            success:function(json){
-                var data = json.data,
-                firstid = data[0].kdgol2;
-
-                $('#kdgol2').html('');
-                for (var i = 0; i < data.length; i++) {
-                    $('#kdgol2').append('<option value="'+data[i].kdgol2+'">'+data[i].nama+'</option>')
-                }
-                
-                $.ajax({
-                    type: "POST",
-                    url: <?php echo '"'.site_url().'admin/inventory/level3/"' ?>+firstid,
-                    success:function(json){
-                        var data = json.data;
-                        
-                        $('#kdgol3').html('');
-                        for (var i = 0; i < data.length; i++) {
-                            $('#kdgol3').append('<option value="'+data[i].kdgol3+'">'+data[i].nama+'</option>')
-                        }
+                    $('#kdgol2').html('');
+                    for (var i = 0; i < data.length; i++) {
+                        $('#kdgol2').append('<option value="'+data[i].kdgol2+'">'+data[i].nama+'</option>')
                     }
-                });
 
-            },
+                    $.ajax({
+                        type: "POST",
+                        url: "<?php echo site_url();?>inventory/level3/"+firstid,
+                        success:function(json){
+                            var data = json.data;
+                            $('#kdgol3').html('');
+                            for (var i = 0; i < data.length; i++) {
+                                $('#kdgol3').append('<option value="'+data[i].kdgol3+'">'+data[i].nama+'</option>')
+                            }
+                        },
+                    });
+                },
+            });
+        });
+        
+        // $('#kdgol').change(function(){
+        //     $.ajax({
+        //         type: "POST",
+        //         url: "<?php echo site_url();?>inventory/level2/"+$(this).val(),
+        //         success:function(json){
+        //             var data = json.data;
+        //             $('#kdgol2').html('');
+        //             for (var i = 0; i < data.length; i++) {
+        //                 $('#kdgol2').append('<option value="'+data[i].kdgol2+'">'+data[i].nama+'</option>')
+        //             }
+        //         },
+        //     });
+        // });
+        
+        $('#kdgol2').change(function(){
+            $.ajax({
+                type: "POST",
+                url: "<?php echo site_url();?>inventory/level3/"+$(this).val(),
+                success:function(json){
+                    var data = json.data;
+                    $('#kdgol3').html('');
+                    for (var i = 0; i < data.length; i++) {
+                        $('#kdgol3').append('<option value="'+data[i].kdgol3+'">'+data[i].nama+'</option>')
+                    }
+                },
+            });
         });
     });
-    
-    // $('#btnSubmit').click(function(event) {
-    //     event.preventDefault();
-    //     $('#frmAddress').submit();
-    // });
-    
-    $('#kdgol').change(function(){
-        $.ajax({
-            type: "POST",
-            url: "<?php echo site_url();?>inventory/level2/"+$(this).val(),
-            // dataType: "json",
-            // data: {"id":$(this).val()},
-            success:function(json){
-                var data = json.data,
-                    firstid = data[0].kdgol2;
-
-                $('#kdgol2').html('');
-                for (var i = 0; i < data.length; i++) {
-                    $('#kdgol2').append('<option value="'+data[i].kdgol2+'">'+data[i].nama+'</option>')
-                }
-
-                $.ajax({
-                    type: "POST",
-                    url: "<?php echo site_url();?>inventory/level3/"+firstid,
-                    success:function(json){
-                        var data = json.data;
-                        $('#kdgol3').html('');
-                        for (var i = 0; i < data.length; i++) {
-                            $('#kdgol3').append('<option value="'+data[i].kdgol3+'">'+data[i].nama+'</option>')
-                        }
-                    },
-                });
-            },
-        });
-    });
-    
-    $('#kdgol2').change(function(){
-        $.ajax({
-            type: "POST",
-            url: "<?php echo site_url();?>inventory/level3/"+$(this).val(),
-            success:function(json){
-                var data = json.data;
-                $('#kdgol3').html('');
-                for (var i = 0; i < data.length; i++) {
-                    $('#kdgol3').append('<option value="'+data[i].kdgol3+'">'+data[i].nama+'</option>')
-                }
-            },
-        });
-    });
-});
 </script>
--- application/views/admin/inventory/backup/index.php
+++ application/views/admin/inventory/backup/index.php
@@ -20,13 +20,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                             <!-- <form class="frmpaging" action="<?= site_url('admin/inventory/setpaging'); ?>" method="post"> -->
                                             <div class="dataTables_length" id="example1_length">
                                             <label>Show <select name="example1_length" aria-controls="example1" class="form-control input-sm">
-                                                <option value="10"<?= (!isset($_SESSION['paging'])) ? '': ($_SESSION['paging']=='10') ? ' selected="selected"':''; ?>>10</option>
-                                                <option value="25"<?= (!isset($_SESSION['paging'])) ? '': ($_SESSION['paging']=='25') ? ' selected="selected"':''; ?>>25</option>
-                                                <option value="50"<?= (!isset($_SESSION['paging'])) ? '': ($_SESSION['paging']=='50') ? ' selected="selected"':''; ?>>50</option>
-                                                <option value="100"<?= (!isset($_SESSION['paging'])) ? '': ($_SESSION['paging']=='100') ? ' selected="selected"':''; ?>>100</option>
+                                                <option value="10"<?php if(isset($_SESSION['paging'])) : ($_SESSION['paging']==='10') ? ' selected':''; endif; ?>>10</option>
+                                                <option value="25"<?php if(isset($_SESSION['paging'])) : ($_SESSION['paging']==='25') ? ' selected':''; endif; ?>>25</option>
+                                                <option value="50"<?php if(isset($_SESSION['paging'])) : ($_SESSION['paging']==='50') ? ' selected':''; endif; ?>>50</option>
+                                                <option value="100"<?php if(isset($_SESSION['paging'])) : ($_SESSION['paging']==='100') ? ' selected':''; endif; ?>>100</option>
                                             </select> entries</label></div><!--</form>--></div>
                                         <div class="col-sm-6">
-                                            <form class="frmfilter" action="<?= site_url('admin/inventory'); ?>" method="get"><div id="search_filter" class="dataTables_filter"><label>Search:<input type="search" name="q" value="<?=isset($_SESSION['q'])?$_SESSION['q']:''; ?>" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></form></div></div>
+                                            <form class="frmfilter" action="<?= site_url('admin/inventory'); ?>" method="get"><div id="search_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></form></div></div>
                                 </div>
                                 <div class="box-body table-responsive">
                                     <table class="table table-striped table-hover">
@@ -40,7 +40,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                 <th><?= lang('inventory_unit'); ?></th>
                                                 <th><?= lang('inventory_brand'); ?></th>
                                                 <th><?= lang('inventory_size'); ?></th>
-                                                <th class="text-right"><?= lang('inventory_price'); ?></th>
                                                 <!-- <th class="text-right">Lebar</th>
                                                 <th class="text-right">Tinggi</th> -->
                                                 <th><?= lang('inventory_picture'); ?></th>
@@ -48,7 +47,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                 <th>Kapasitas</th>
                                                 <th>Gas</th>
                                                 <th>Berat</th>
-                                                <th>Tag</th> -->
+                                                <th>Tag</th>
+                                                <th>Harga Jual</th>
+                                                <th>Diskon</th> -->
                                                 <th>Action</th>
                                             </tr>
                                         </thead>
@@ -63,10 +64,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                 <td><?php echo htmlspecialchars($item->satuan, ENT_QUOTES, 'UTF-8'); ?></td>
                                                 <td><?php echo htmlspecialchars($item->merk, ENT_QUOTES, 'UTF-8'); ?></td>
                                                 <td><?php echo $item->pnj.' cm<br>x<br>'.$item->lbr.' cm<br>x<br>'.$item->tgi.' cm'; ?></td>
-                                                <td class="text-right"><?php echo htmlspecialchars($item->hjual, ENT_QUOTES, 'UTF-8'); ?></td>
                                                 <!-- <td class="text-right"><?php echo htmlspecialchars($item->lbr, ENT_QUOTES, 'UTF-8'); ?></td>
                                                 <td class="text-right"><?php echo htmlspecialchars($item->tgi, ENT_QUOTES, 'UTF-8'); ?></td> -->
                                                 <td><div class="product-img"><img src="<?=site_url($this->data['products_dir'].'/'.$item->gambar)?>" alt="Image"></div></td>
+                                                <!-- <td><?php echo htmlspecialchars($item->listrik, ENT_QUOTES, 'UTF-8'); ?></td>
+                                                <td><?php echo htmlspecialchars($item->kapasitas, ENT_QUOTES, 'UTF-8'); ?></td>
+                                                <td><?php echo htmlspecialchars($item->gas, ENT_QUOTES, 'UTF-8'); ?></td>
+                                                <td><?php echo htmlspecialchars($item->berat, ENT_QUOTES, 'UTF-8'); ?></td>
+                                                <td><?php echo htmlspecialchars($item->tag, ENT_QUOTES, 'UTF-8'); ?></td>
+                                                <td><?php echo htmlspecialchars($item->hjual, ENT_QUOTES, 'UTF-8'); ?></td>
+                                                <td><?php echo htmlspecialchars($item->disc, ENT_QUOTES, 'UTF-8'); ?></td> -->
                                                 <td>
                                                     <?php echo anchor('admin/inventory/edit/'.$item->kdurl, 'Edit'); ?>
                                                 </td>
@@ -97,6 +104,12 @@ $(document).ready(function(){
                 location.reload();
             }
         });
+    });
+    
+    $(".dataTables_filter").on('keyup', function (e) {
+        if (e.keyCode == 13) {
+        alert('tes')
+        }
     });
 });
 </script>
